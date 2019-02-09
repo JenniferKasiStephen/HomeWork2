15a428568272951a11819311a584e936fb668b88
JenniferKasiStephen <47457925+JenniferKasiStephen@users.noreply.github.com>
2019-02-08 21:08:44 -0500

    Update Container.java

--- src/Container/Container.java
+++ src/Container/Container.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 	public class Container {
 
-		public static void main(String[] args) {
+	    public static void main(String[] args) {
 	    System.out.println("Enter number: ");
 	    Scanner sc = new Scanner(System.in);
 	    int[]  value = new int[9];
-	    for(int i = 0 ;i<10; i++) {
-	     value[i] = sc.nextInt();
+	    for(int i = 0 ;i<9; i++) {
+	    value[i] = sc.nextInt();
 	    }
-	      System.out.println(value[1]);
-             sc.close();
-
-	   }
+	    for(int j=1; j<9; j++) {
+	    System.out.println(value[j]);
+	    sc.close();
+	    }
 	}
 
 
